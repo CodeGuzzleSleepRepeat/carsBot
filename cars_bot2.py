577605bc02d7f8aea0c21716e6de8d6e1340dbe0
CodeGuzzleSleepRepeat <66034655+CodeGuzzleSleepRepeat@users.noreply.github.com>
2022-04-06 20:24:13 +0300

    Update cars_bot2.py

--- cars_bot2.py
+++ cars_bot2.py
@@ -751,6 +751,8 @@ def get_mes_by_time(manager, day, chat_id):
 		if a == '':
 			continue
 		words = a.split(';;')
+		
+		print(wordwords[2][:8], day, words[2][:8] == day)
 		if words[1].lower() == manager.lower() and words[2][:8] == day:
 			try:
 				mes[words[2]] += str(words[2]) + ': ' + str(words[0])+ '\n'
